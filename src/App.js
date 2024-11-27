ccf46ee9852120910b610c2a3b9932d9c445022c
[abhiraj] <abhiraj.dustakar@gmail.com>
2024-11-27 23:27:04 +0530

    updated signup page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar.component.jsx";
 import Home from "./pages/Home_Page/Home.component.jsx";
 import Update from "./pages/update_page/Update.jsx";
+import Login from "./pages/LoginPage/Login.jsx";
+import SignUp from "./pages/SignUp_Page/signup.component.jsx";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -33,6 +35,8 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/addevent" element={<Form />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<SignUp />} />
               <Route path="/viewevents" element={<View />} />
               <Route path="/Update/:id" element={<Update />} />
             </Routes>
--- src/pages/SignUp_Page/signup.component.jsx
+++ src/pages/SignUp_Page/signup.component.jsx
@@ -1,23 +1,27 @@
 import React, { useState } from "react";
 import api from "../api";
 import "./signup.css"
+import axios from "axios";
+
 const SignUp = () => {
-  const [formData, setFormData] = useState({ username: "", password: "", email: "" });
+  const [formData, setFormData] = useState({ name: "", password: "", email: "" });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      await api.post("/auth/signup", formData);
-      alert("User registered successfully!");
-    } catch (error) {
-      console.error("Registration failed:", error);
-      alert("user registration unsuccessfull" + error);
-    }
+   axios
+               .post("http://localhost:8080/api/auth/register", formData)
+               .then((resp) => {
+                   console.log(resp.data);
+                   alert("User added successfully!");
+               })
+               .catch((error) => {
+                   alert("Error adding User: " + error);
+               });
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="Username" onChange={(e) => setFormData({ ...formData, username: e.target.value })} />
+      <input type="text" placeholder="name" onChange={(e) => setFormData({ ...formData, name: e.target.value })} />
       <input type="password" placeholder="Password" onChange={(e) => setFormData({ ...formData, password: e.target.value })} />
       <input type="email" placeholder="Email" onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
       <button type="submit">Sign Up</button>
